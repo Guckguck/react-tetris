08cf9123889f2cc316968b7317489eaa35f3c1dd
Heinrich Klink <Heinrich.Klink@docc.Techstarter.de>
2024-01-30 19:17:50 +0100

    Added Dockerfile and github-actions to test.yml

--- app/App.tsx
+++ app/App.tsx
@@ -45,6 +45,56 @@ const SubTitle = styled.h2`
   font-size: 18px;
 `;
 
+// const Tetris = require('react-tetris');
+
+// const App = () => (
+//   <div>
+//     <h1>Tetris</h1>
+//     <Tetris
+//       keyboardControls={{
+//         // Default values shown here. These will be used if no
+//         // `keyboardControls` prop is provided.
+//         down: 'MOVE_DOWN',
+//         left: 'MOVE_LEFT',
+//         right: 'MOVE_RIGHT',
+//         space: 'HARD_DROP',
+//         z: 'FLIP_COUNTERCLOCKWISE',
+//         x: 'FLIP_CLOCKWISE',
+//         up: 'FLIP_CLOCKWISE',
+//         p: 'TOGGLE_PAUSE',
+//         c: 'HOLD',
+//         shift: 'HOLD'
+//       }}
+//     >
+//       {({
+//         HeldPiece,
+//         Gameboard,
+//         PieceQueue,
+//         points,
+//         linesCleared,
+//         state,
+//         controller
+//       }) => (
+//         <div>
+//           <HeldPiece />
+//           <div>
+//             <p>Points: {points}</p>
+//             <p>Lines Cleared: {linesCleared}</p>
+//           </div>
+//           <Gameboard />
+//           <PieceQueue />
+//           {state === 'LOST' && (
+//             <div>
+//               <h2>Game Over</h2>
+//               <button onClick={controller.restart}>New game</button>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//     </Tetris>
+//   </div>
+// );
+
 const App = (): JSX.Element => (
   <Container>
     <Header>
